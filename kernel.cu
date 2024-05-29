145a1cdff89d167ae500d30e3951ffb95e5e2d8e
Ruth Sun <rsun@bender.engr.ucr.edu>
2024-05-29 13:17:41 -0700

    skeleton for kernel

--- kernel.cu
+++ kernel.cu
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define BLOCK_SIZE 1024
+#define GRID_SIZE 36
+
 // terrible implementation. 4 parameters, really-
 __device__ void numToIndex(int num, int numChars, int pswdLen, int* arr) {
     int iterator = pswdLen - 1;
@@ -14,6 +17,26 @@ __device__ void numToIndex(int num, int numChars, int pswdLen, int* arr) {
     }
 }
 
+__global__ void crack_kernel(char* validChars, int numValidChars, int pswdLen) {
+    int startingNum, endingNum;
+    startingNum = ;
+
+    int* startingIndex, endingIndex;
+    numToIndex(startingNum, numValidChars, pswdLen, startingIndex);
+    numToIndex(endingNum, numValidChars, pswdLen, endingIndex);
+    
+    while(false) { // replace condition later
+        // create password
+        // update index
+    }
+}
+
+void crack(char* validChars, int numValidChars, int pswdLen) {
+    dim3 blockSize (1024, 1, 1);
+    dim3 gridSize (36, 1, 1);
+
+    crack_kernel<<<gridSize, blockSize>>>(validChars, numValidChars, pswdLen);
+}
 __device__ void incrementIndex(int* index, int base) {
     int overflow = 1;
     for (int i = base - 1; i >= 0; --i){
